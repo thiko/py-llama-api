daff115ebcfabbfbb9a28111afe424974839cea0
Thimo Koenig <thimo.koenig@gmail.ch>
2023-04-01 21:49:38 +0200

    small python FastAPI application using llama index to make use of personal documents with GPT

---
+++ server.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from llama import LlamaQuestionnaire
+
+app = FastAPI()
+llama_api = LlamaQuestionnaire()
+
+
+@app.get("/gpt")
+async def ask_gpt(question: str):
+    print("Got question: " + question)
+    return llama_api.ask_gpt(question)
